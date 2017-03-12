940c4178933160826ebba24035be6b0417f2e5c2
pai-55 <pai-55@sr.dmcs.pl>
2017-03-12 19:59:32 +0100

    updated menu

--- app/src/main/java/com/example/pc/calculator/Menu.java
+++ app/src/main/java/com/example/pc/calculator/Menu.java
@@ -1,13 +1,81 @@
 package com.example.pc.calculator;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+
+    private Button simple;
+    private Button advanced;
+    private Button about;
+    private Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        loadButtons();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.simple:
+                simpleOnClick();
+                break;
+            case R.id.advanced:
+                advancedOnClick();
+                break;
+            case R.id.about:
+                aboutOnClick();
+                break;
+            case R.id.exit:
+                exitOnClick();
+                break;
+        }
+
+    }
+
+    private void loadButtons() {
+        simple = (Button) findViewById(R.id.simple);
+        advanced = (Button) findViewById(R.id.advanced);
+        about = (Button) findViewById(R.id.about);
+        exit = (Button) findViewById(R.id.exit);
+
+        simple.setOnClickListener(this);
+        advanced.setOnClickListener(this);
+        about.setOnClickListener(this);
+        exit.setOnClickListener(this);
+    }
+
+    private void simpleOnClick() {
+
+    }
+
+    private void advancedOnClick() {
+
+    }
+
+    private void aboutOnClick() {
+        AlertDialog alertDialog = new AlertDialog.Builder(Menu.this).create();
+        alertDialog.setTitle("About");
+        alertDialog.setMessage("\nApp made by Bartosz Iwaszkiewicz\n\n187705 WEEiA\n");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "CLOSE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private void exitOnClick() {
+
     }
 }
